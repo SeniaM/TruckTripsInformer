ddb544d159369b4546a32f74fc91b87ad618b025
SenyaM <118691376+SeniaM@users.noreply.github.com>
2024-06-13 20:54:00 +0800

    add chatCommand "/tti"

--- TruckTripsInformer.lua
+++ TruckTripsInformer.lua
@@ -4,7 +4,7 @@ script_author("")
 script_description("Задача скрипта показывать актуальную информацию о заказах и заработке")
 
 require "lib.moonloader" 
-
+    
 local sampev = require ("lib.samp.events")
 local encoding = require 'encoding'
 local string = require 'string'
@@ -21,69 +21,74 @@ local sumForEd
 local sumEarn
 local decodedString
 
-local isPrintStat = false
-local isDialog1   = false
-local isDialog2   = false
-local isShowDial  = false
+local isPrintStat      = false
+local isDialog1        = false
+local isDialog2        = false
+local isShowDial       = false
+local isScriptActivate = false
+
 encoding.default  = 'cp1251' -- set the same encoding as script file
 u8 = encoding.UTF8
 
 function sampev.onServerMessage(color, text)
-    if text:find("Вы загрузили") and color == 865730559 then
-        pickEd = tonumber(string.match(text, "{FFAA00}(%d+)"))
-        if percent ~=0 then
-            sumEarn = (pickEd * sumForEd) - ((pickEd * sumForEd) * percent / 100)
-        else
-            sumEarn = (pickEd * sumForEd)
+    if isScriptActivate == true then
+        if text:find("Вы загрузили") and color == 865730559 then
+            pickEd = tonumber(string.match(text, "{FFAA00}(%d+)"))
+            if percent ~=0 then
+                sumEarn = (pickEd * sumForEd) - ((pickEd * sumForEd) * percent / 100)
+            else
+                sumEarn = (pickEd * sumForEd)
+            end
+            isDialog2 = true
         end
-        isDialog2 = true
-    end
-    if text:find("Вы привезли") and color == 865730559 then
-        local file = io.open(getGameDirectory().."//moonloader//file.txt", "a")
-        file:write(text)
-        file:close()
+        if text:find("Вы привезли") and color == 865730559 then
+            local file = io.open(getGameDirectory().."//moonloader//file.txt", "a")
+            file:write(text)
+            file:close()
 
-        local pattern1 = "{FFAA00}(%d+)"
-        local res1 = string.match(text, pattern1)
-        local money = tonumber(string.match(text, "{00cc99}(%d+)"))
-        local commissionInDollar = tonumber(string.match(text, "{ff8080}(%d+)"))
-        
-        if commissionInDollar ~= 0 then
-            percent = math.floor(commissionInDollar / (money / 100))
+            local pattern1 = "{FFAA00}(%d+)"
+            local res1 = string.match(text, pattern1)
+            local money = tonumber(string.match(text, "{00cc99}(%d+)"))
+            local commissionInDollar = tonumber(string.match(text, "{ff8080}(%d+)"))
+            
+            if commissionInDollar ~= 0 then
+                percent = math.floor(commissionInDollar / (money / 100))
+            end
+            moneyBox = moneyBox + money
+            numberOfTrips = numberOfTrips + 1
+
+            isPrintStat = true
         end
-        moneyBox = moneyBox + money
-        numberOfTrips = numberOfTrips + 1
-
-        isPrintStat = true
     end
-
 end
 
 function onSendRpc(id, bitStream, priority, reliability, orderingChannel, shiftTs)
-    if id == 62 then
-        local wDialogID = raknetBitStreamReadInt16(bitStream)
-        if wDialogID == 3079 then
-            local bResponse    = raknetBitStreamReadInt8(bitStream)
-            local wListItem    = raknetBitStreamReadInt16(bitStream)
-            local bTextLength  = raknetBitStreamReadInt8(bitStream)
-                  Text         = raknetBitStreamReadString(bitStream, bTextLength)
-            local textItem     = sampGetListboxItemText(wListItem)
-            local res          = get_line_from_list(decodedString, wListItem + 1)
-            local pattern1 = "(%d+$)"
+    if isScriptActivate == true then
+        if id == 62 then
+            local wDialogID = raknetBitStreamReadInt16(bitStream)
+            if wDialogID == 3079 then
+                local bResponse    = raknetBitStreamReadInt8(bitStream)
+                local wListItem    = raknetBitStreamReadInt16(bitStream)
+                local bTextLength  = raknetBitStreamReadInt8(bitStream)
+                    Text         = raknetBitStreamReadString(bitStream, bTextLength)
+                local textItem     = sampGetListboxItemText(wListItem)
+                local res          = get_line_from_list(decodedString, wListItem + 1)
+                local pattern1 = "(%d+$)"
 
-            local pattern2 = "(%d+)/" 
-            local pattern3 = "/(%d+)"
-            
-            Text = string.sub(Text, 1, #Text-4)
-            local sum = string.match(res, pattern1)
-            sum = string.sub(sum, 1, #sum-1)
-            local ed1 = string.match(res, pattern2)
-            local ed2 = string.match(res, pattern3)
+                local pattern2 = "(%d+)/" 
+                local pattern3 = "/(%d+)"
+                
+                Text = string.sub(Text, 1, #Text-4)
+                local sum = string.match(res, pattern1)
+                sum = string.sub(sum, 1, #sum-1)
+                local ed1 = string.match(res, pattern2)
+                local ed2 = string.match(res, pattern3)
 
-            sumForEd = tonumber(sum)
+                sumForEd = tonumber(sum)
 
-            if bResponse == 1 then
-                isDialog1 = true
+                if bResponse == 1 then
+                    isDialog1 = true
+                end
             end
         end
     end
@@ -111,6 +116,7 @@ function main()
          return end
     while not isSampAvailable() do wait(100) end
     
+    sampRegisterChatCommand("tti", activateScript)
     sampAddChatMessage("Truck Trips Informer загружен!", 0xFFFF00)
     
 
@@ -154,4 +160,14 @@ end
 function printStat()
     sampSendChat(string.format("/do Пета сдал груз. Статистика на текущий момент:"))
     sampSendChat(string.format("/do Завершенных поездок: " .. tostring(numberOfTrips) .. ". Заработано налички: $" .. tostring(moneyBox)))
+end
+
+function activateScript(arg)
+    isScriptActivate = not isScriptActivate
+    
+    if isScriptActivate == true then
+        sampAddChatMessage("Truck Trips Informer: {34bf49}[enabled]", 0x0099e5)
+    else
+        sampAddChatMessage("Truck Trips Informer: {ff4c4c}[diabled]", 0x0099e5)
+    end
 end
