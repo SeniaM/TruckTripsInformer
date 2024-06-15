d7217bcdfb8ff2dcdbf8190cdfe25d0b23fd9416
SenyaM <118691376+SeniaM@users.noreply.github.com>
2024-06-16 04:08:06 +0800

    added module measure time of trip

--- TruckTripsInformer.lua
+++ TruckTripsInformer.lua
@@ -9,6 +9,7 @@ local sampev = require ("lib.samp.events")
 local encoding = require 'encoding'
 local string = require 'string'
 
+local filePathDuration = getGameDirectory().."\\moonloader\\durationoftrip.txt"
 ---------Stats--------
 local moneyBox = 0
 local numberOfTrips = 0
@@ -20,7 +21,12 @@ local pickEd
 local sumForEd
 local sumEarn
 local decodedString
-
+--------Timer----------
+local timeOfTripFromFile
+local startTimer
+local endTimer
+local currentTimeTrip
+--------EndTimer----------
 local isPrintStat      = false
 local isDialog1        = false
 local isDialog2        = false
@@ -33,33 +39,47 @@ u8 = encoding.UTF8
 function sampev.onServerMessage(color, text)
     if isScriptActivate == true then
         if text:find("Вы загрузили") and color == 865730559 then
-            local file = io.open(getGameDirectory().."//moonloader//durationoftrip.txt", "a+")
-            local isComp = false
-            for line in file:lines() do
-                if isCompareText(Text .. "=0", line) then
-                    isComp = true
-                end    
-            point1, point2 = splitPointfromText(line)
-            sampAddChatMessage(point1, 0x0099e5)
-            sampAddChatMessage(point2, 0x0099e5)
-            point3, point4 = splitPointfromText(string.format(Text .. "=0"))       
-            end
-            sampAddChatMessage(point3, 0x0099e5)
-            sampAddChatMessage(point4, 0x0099e5)   
-            if isComp == false then
-            file:write(Text .. "=0\n")
-            end
-            file:close()
+            startTimer = os.clock()
+            
         ----------------------------------------------------------------
             pickEd = tonumber(string.match(text, "{FFAA00}(%d+)"))
             if percent ~=0 then
-                sumEarn = (pickEd * sumForEd) - ((pickEd * sumForEd) * percent / 100)
+                sumEarn = tonumber((pickEd * sumForEd) - ((pickEd * sumForEd) * percent / 100))
             else
-                sumEarn = (pickEd * sumForEd)
+                sumEarn = pickEd * sumForEd
             end
             isDialog2 = true
         end
         if text:find("Вы привезли") and color == 865730559 then
+            endTimer = os.clock()
+            currentTimeTrip = endTimer - startTimer
+            local isComp = false
+            local content = readFileToTable(filePathDuration)    
+            for i, line in ipairs(content) do
+                if isCompareText(Text .. "=0", line) then
+                    isComp = true
+                    timeOfTripFromFile = tonumber(string.match(line ,"=(%d*)"))
+                    if timeOfTripFromFile < currentTimeTrip then
+                        content[i] = Text .. "=" .. currentTimeTrip
+                    end
+                    sampAddChatMessage(timeOfTripFromFile, 0x0099e5)
+                end    
+                point1, point2 = splitPointfromText(line)
+                sampAddChatMessage(point1, 0x0099e5)
+                sampAddChatMessage(point2, 0x0099e5)
+                point3, point4 = splitPointfromText(string.format(Text .. "=0"))       
+            end
+
+            sampAddChatMessage(point3, 0x0099e5)
+            sampAddChatMessage(point4, 0x0099e5)   
+            if isComp == true then
+                saveTableToString(filePathDuration, content)
+            else
+                local file = io.open(filePathDuration,"a+")
+                file:write(Text .. "=" .. currentTimeTrip .. "\n")
+                file:close()
+            end
+            ---------------------------------------------------------------
             local pattern1 = "{FFAA00}(%d+)"
             local res1 = string.match(text, pattern1)
             local money = tonumber(string.match(text, "{00cc99}(%d+)"))
@@ -81,26 +101,28 @@ function onSendRpc(id, bitStream, priority, reliability, orderingChannel, shiftT
         if id == 62 then
             local wDialogID = raknetBitStreamReadInt16(bitStream)
             if wDialogID == 3079 then
-                local bResponse    = raknetBitStreamReadInt8(bitStream)
-                local wListItem    = raknetBitStreamReadInt16(bitStream)
-                local bTextLength  = raknetBitStreamReadInt8(bitStream)
-                    Text         = raknetBitStreamReadString(bitStream, bTextLength)
-                local textItem     = sampGetListboxItemText(wListItem)
-                local res          = get_line_from_list(decodedString, wListItem + 1)
-                local pattern1 = "(%d+$)"
-
-                local pattern2 = "(%d+)/" 
-                local pattern3 = "/(%d+)"
-                
-                Text = string.sub(Text, 1, #Text-4)
-                local sum = string.match(res, pattern1)
-                sum = string.sub(sum, 1, #sum-1)
-                local ed1 = string.match(res, pattern2)
-                local ed2 = string.match(res, pattern3)
-
-                sumForEd = tonumber(sum)
-
+                local bResponse = raknetBitStreamReadInt8(bitStream)
                 if bResponse == 1 then
+                    local wListItem    = raknetBitStreamReadInt16(bitStream)
+                    local bTextLength  = raknetBitStreamReadInt8(bitStream)
+                        Text         = raknetBitStreamReadString(bitStream, bTextLength)
+                    local textItem     = sampGetListboxItemText(wListItem)
+                    local res          = get_line_from_list(decodedString, wListItem + 1)
+                    local pattern1 = "%s(%d*)%$"
+
+                    local pattern2 = "(%d*)/" 
+                    local pattern3 = "/(%d*)"
+                    
+                    Text = string.sub(Text, 1, #Text-4)
+                    local sum = tonumber(string.match(res, pattern1))
+                    --sum = string.sub(sum, 1, #sum-1)
+                    local ed1 = string.match(res, pattern2)
+                    local ed2 = string.match(res, pattern3)
+                    sampAddChatMessage(res, 0x0099e5)
+                    sampAddChatMessage(sum, 0x0099e5)
+                    sampAddChatMessage(ed1, 0x0099e5)
+                    sampAddChatMessage(ed2, 0x0099e5)
+                    sumForEd = sum
                     isDialog1 = true
                 end
             end
@@ -121,6 +143,9 @@ function onReceiveRpc(id, bs)
             local bButton2Len     = raknetBitStreamReadInt8(bs)
             local szButton2       = raknetBitStreamReadString(bs, bButton2Len)
             decodedString = raknetBitStreamDecodeString(bs, 4096)
+            local file = io.open(getGameDirectory().."//moonloader//List.txt", "a+") --remove this
+            file:write(decodedString)
+            file:close()
         end
    end
 end
@@ -135,7 +160,8 @@ function main()
     
 
     while true do wait(0)
-        if isKeyJustPressed(VK_CONTROL) then
+        if isKeyJustPressed(VK_1) then
+        
         end
 
         if isDialog1 == true and isDialog2 == true then
@@ -165,14 +191,15 @@ function get_line_from_list(str, line_number)
 end
 
 function sayTextInfo(order, amount, price)
-    sampSendChat(string.format("/do Пета взял заказ со следующими характеристиками: "))
     sampSendChat(string.format("/do Маршрут: %s", order))
-    sampSendChat(string.format("/do Стоимостью $%s за единицу весом %sкг на общую сумму $%s", tostring(sumForEd), tostring(amount), tostring(price)))
+    sampSendChat(string.format("/do Стоимостью $%s за единицу весом %sкг на сумму $%s", tostring(sumForEd), tostring(amount), tostring(price)))
     sampSendChat(string.format("/do Ожидаемая общая прибыль составит: $%d", price + moneyBox))
 end
 
 function printStat()
-    sampSendChat(string.format("/do Пета сдал груз. Статистика на текущий момент:"))
+    sampSendChat(string.format("/do Вы сдали груз. Статистика на текущий момент:"))
+    sampSendChat(string.format("/do Потребовалось времени на доставку: " .. math.floor(currentTimeTrip/60) .. " мин. " 
+                                                                         .. math.floor(currentTimeTrip) - (math.floor(currentTimeTrip/60) * 60) .. " сек."))
     sampSendChat(string.format("/do Завершенных поездок: " .. tostring(numberOfTrips) .. ". Заработано налички: $" .. tostring(moneyBox)))
 end
 
@@ -202,4 +229,34 @@ function isCompareText(str1, str2)
     else
         return false
     end
+end
+
+function readFileToTable(filePath)
+    local file = io.open(filePath, "r")
+    if not file then
+        print("Не удалось открыть файл для чтения")
+        return nil
+    end
+
+    local content = {}
+    for line in file:lines() do
+        table.insert(content, line)
+    end
+    file:close()
+    return content
+end
+
+function saveTableToString(filePath, table)
+    local file = io.open(filePath, "w")
+    if not file then
+        print("Не удалось открыть файл для записи")
+        return false
+    end
+
+    for _, value in ipairs(table) do
+        file:write(value .. "\n")
+    end
+
+    file:close()
+    return true
 end
